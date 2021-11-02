994768497bc713c5546c507ba471ddc9e75294b1
mcsimrus <mcsimrus@gmail.com>
2021-11-02 23:30:29 +0300

    Changed GET parameters to dictionary

--- weather.py
+++ weather.py
@@ -7,11 +7,15 @@ def main():
 
     for location in locations:
         
-        url = 'https://wttr.in/{}?Tqnm&lang=ru'.format(location)
+        #url = 'https://wttr.in/{}?Tqnm&lang=ru'.format(location)
+        url = 'https://wttr.in/{}'.format(location)
 
-        response = requests.get(url)
+        payload = {'key':'Tqnm','lang':'ru'}
+                
+        response = requests.get(url, params=payload)
         response.raise_for_status()
         print(response.text)
+        #print(response.url)
 
 if __name__ == '__main__':
     main()
