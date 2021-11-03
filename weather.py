64a1ae60e227103c6f323413b99a393c7075fd18
mcsimrus <37913906+mcsimrus@users.noreply.github.com>
2021-11-03 15:49:18 +0300

    deleted unnecessary comments

--- weather.py
+++ weather.py
@@ -7,15 +7,13 @@ def main():
 
     for location in locations:
         
-        #url = 'https://wttr.in/{}?Tqnm&lang=ru'.format(location)
         url = 'https://wttr.in/{}'.format(location)
 
-        payload = {'key':'Tqnm','lang':'ru'}
+        payload = {'Tqnm':'','lang':'ru'}
                 
         response = requests.get(url, params=payload)
         response.raise_for_status()
         print(response.text)
-        #print(response.url)
 
 if __name__ == '__main__':
     main()
