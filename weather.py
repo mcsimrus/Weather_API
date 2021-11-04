bc9019170ae04607b2071bcb30c830744b114073
mcsimrus <mcsimrus@gmail.com>
2021-11-04 07:00:33 +0300

    Fixed pep8 errors

--- weather.py
+++ weather.py
@@ -6,11 +6,9 @@ def main():
     locations = ('London', 'SVO', 'Череповец')
 
     for location in locations:
-        
         url = 'https://wttr.in/{}'.format(location)
 
-        payload = {'Tqnm':'','lang':'ru'}
-                
+        payload = {'Tqnm': '', 'lang': 'ru'}
         response = requests.get(url, params=payload)
         response.raise_for_status()
         print(response.text)
